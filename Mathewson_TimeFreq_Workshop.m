eaff3bb66577c77ff40d85071c65e2ce1b5d1be0
Kyle Mathewson <kylemath@gmail.com>
2019-05-14 15:20:31 -0600

    removed the wavelet vs fft tester

--- Mathewson_TimeFreq_Workshop.m
+++ Mathewson_TimeFreq_Workshop.m
@@ -359,122 +359,6 @@ subplot(3,1,3); imagesc(epoch_times,freqs_wavelet,squeeze(power_wavelet(1,:,:)))
     ylabel(c,'Log Power (dB)')
     
 
-
-%% Wavelet Tester Examples
-
-% Now that we have covered wavelet and fft analysis, we can compare the two
-% directly and check what happens as we change the settings. To do this we
-% will make new fake data with simulated oscillations at different
-% frequencies as see how well we can resolve those frequencies in our
-% analyses
-
-
-%random normal data
-noise_level = 10;
-data = randn(1,1000)*noise_level;
-t = 0:1:length(data)-1;% Time Samples
-fs = 200;% Sampling Frequency
-
-% Create sine wave
-f1 = 20;% Input Signal Frequency
-d1 = sin(2*pi*(f1/fs)*t)*(randi(100));% Generate Sine Wave  
-f2 = 20;
-d2 = sin(2*pi*(f2/fs)*t)*(randi(100));
-
-%Modulators - If we want to simulate high frequency modulated by low
-f3 = 2;
-f4 = 1;
-d3 = sin(2*pi*(f3/fs)*t)*(randi(10));% Generate Sine Wave  
-d4 = sin(2*pi*(f4/fs)*t)*(randi(10));% Generate Sine Wave  
-
-% data = data+(d1);
-% data = data+(d1+d2); %adjust these to change signals
-data = data+(d1.*d3); %for modulations
-% data = data+(d1.*d3)+(d2.*d4);
-
-%Try putting your own data and srate here
-% data = EEG_P3_WFilt(end-10000:end,2)'; 
-% fs = srate;
-
-% Assign Frequencies of interest
-% F = 1:.5:100; %linear
-F = (2^(1/4)).^(-.5:.5:20); %Log 
-
-% width of the wavelet
-width = 3; 
-
-% Compute Wavelet Analsis
-tic;
-[B,T,P] = BOSC_tf(data,F,fs,width);
-toc;
-
-% B = log10(B); %log transform
-
-% use this to try subtracting the mean from the spectra
-mean_spectra = mean(B,2);
-B = B-repmat(mean_spectra,1,size(B,2));
-
-% Compute FFT Analysis
-[pow, phs, freq] = kyle_fft(data,fs,fs/2);
-
-% Plot Data
-figure;
-
-%Plot Original Time Series
-subplot(3,3,1:2); plot(T,data); axis tight; title('Raw Time Series');
-xlabel('Time[s]');
-ylabel('Voltage[uV]');
-
-%plot the FFT spectra
-subplot(3,3,3);
-plot(1:length(freq),pow); title('FFT Spectra');
-xf=1:10000:length(freq); % to get log-sampled frequency tick marks
-set(gca,'XTick',xf,'XTickLabel',round(freq(xf)*100)/100);
-ylabel('Log(Power) [dB]');
-xlabel('Frequency [Hz]');
-view(90,90)
-set(gca,'XDir','reverse');
-axis tight;
-
-%Plot The mean Spectra
-subplot(3,3,6); 
-xf=1:4:length(F); % to get log-sampled frequency tick marks
-plot(1:length(F),mean_spectra); title('Mean Wavelet Spectra');
-set(gca,'XTick',xf,'XTickLabel',round(F(xf)*100)/100);
-ylabel('Log(Power) [dB]');
-xlabel('Frequency [Hz]');
-view(90,90)
-set(gca,'XDir','reverse');
-axis tight
-
-%Plot the time-frequency transform
-subplot(3,3,4:5); 
-imagesc(T,1:size(B,1),B); title('Spectrogram');
-colormap(hot);
-set(gca,'YTick',xf,'YTickLabel',round(F(xf)*100)/100);
-ylabel('Frequency [Hz]')
-xlabel ('Time [s]')
-axis xy
-
-%plot the phase for each frequency over time
-subplot(3,3,7:8); 
-imagesc(T,1:size(P,1),P); title('Phase Spectrogram');
-colormap(hot);
-set(gca,'YTick',xf,'YTickLabel',round(F(xf)*100)/100);
-ylabel('Frequency [Hz]')
-xlabel ('Time [s]')
-axis xy
-
-subplot(3,3,9); 
-plot(1:length(freq),phs); title('FFT Phase');
-xf=1:25:length(freq); % to get log-sampled frequency tick marks
-set(gca,'XTick',xf,'XTickLabel',round(freq(xf)*100)/100);
-ylabel('Phase [rad]');
-xlabel('Frequency [Hz]');
-view(90,90)
-set(gca,'XDir','reverse');
-axis tight;
-
 %% Power
 % 	Now that we have a few ways of computing the power and phase of our EEG
 % 	signals, we may want to test questions we have about the difference in
@@ -523,8 +407,7 @@ end
 %  circ_stat toolbox for doing stats on circular variables
 %  <https://www.jstatsoft.org/article/view/v031i10 Circ_stat toolbox>
 %   circ_stat()
-% 	circ_mea
-n(), r(), 
+% 	circ_mean(), r(), 
 % 	circular difference, circular clustering
 %   Most of these stats are based on the angular mean and angular standard
 %   deviation, as are more advanced EEG stats mentioned below (coherence)
